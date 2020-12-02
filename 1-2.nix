6b1f05fd4bb958f36610e406bc7115cd8001af9d
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-12-02 07:49:19 -0300

    Record program for day 1, task 2

---
+++ 1-2.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib }:
+with lib;
+let
+  input = pipe ./1-input.txt [
+    readFile
+    (splitString "\n")
+    (filter (s: s != ""))
+    (map toInt)
+  ];
+in
+pipe [ input input ] [
+  (crossLists (x1: x2: { sum = x1 + x2; elements = [ x1 x2 ]; }))
+  (filter (line: line.sum < 2020))
+  (concatMap (line:
+    let candidates = filter (x: ! elem x line.elements && line.sum + x == 2020) input;
+    in map (candidate: line.elements ++ [ candidate ]) candidates))
+  head
+  (foldl' builtins.mul 1)
+]
