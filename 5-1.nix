ef8c8ca2cce13132db5b1e2595887bc01176c9bb
Aluísio Augusto Silva Gonçalves <aluisio@aasg.name>
2020-12-08 09:56:44 -0300

    Record inputs and program for day 5, task 1

---
+++ 5-1.nix
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2020 Aluísio Augusto Silva Gonçalves <https://aasg.name>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+{ pkgs ? import <nixpkgs> { }
+, aasg-nixexprs ? fetchTarball "https://git.sr.ht/~aasg/nixexprs/archive/master.tar.gz"
+}:
+with import "${aasg-nixexprs}/lib/extension.nix" { inherit (pkgs) lib; };
+pipe ./5-input.txt [
+  readFile
+  (splitString "\n")
+  (filter (s: s != ""))
+  (map (spec: foldl'
+    (state: action:
+      let
+        rp = (state.rl + state.rh) / 2;
+        cp = (state.cl + state.ch) / 2;
+      in
+      if action == "F" then state // { rh = rp; }
+      else if action == "B" then state // { rl = rp + 1; }
+      else if action == "L" then state // { ch = cp; }
+      else if action == "R" then state // { cl = cp + 1; }
+      else throw "unknown action ${action}")
+    { rl = 0; rh = 127; cl = 0; ch = 7; }
+    (stringToCharacters spec)
+  ))
+  (map (state:
+    if state.rl != state.rh then throw "row did not converge: ${toString state.rl}, ${toString state.rh}"
+    else if state.cl != state.ch then throw "column did not converge: ${toString state.cl}, ${toString state.ch}"
+    else { row = state.rl; column = state.cl; }))
+  (map ({ row, column }: row * 8 + column))
+  (sort (a: b: a > b))
+  head
+]
